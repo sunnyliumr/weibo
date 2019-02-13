8d01e84ee7c8ff827c63f441b7e31d90d05f2908
sunnyliumr <sunnyliumr@gmail.com>
2019-02-13 11:22:58 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'StaticPagesController@home');
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('sign');
+Route::resource('users', 'UsersController');
+
+
